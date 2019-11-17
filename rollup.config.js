60e78627055f3bad8ef4fa739f130df5466389a7
tongchia <tongchia@live.com>
2019-11-18 02:18:46 +0800

    fix path

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,15 @@ import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import path from 'path';
 
 export default {
   inlineDynamicImports: false,
   input: 'src/main.js',
   output: {
     format: 'amd',
+    sourcemap: true,
+    sourcemapPathTransform: _path => {path.relative('src', _path),
   },
   external: ['jquery', 'bootstrap'],
   plugins: [
